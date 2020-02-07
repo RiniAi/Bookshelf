90c05d9591c966cdce30a2fb22ecbdbbe31ea208
Anna <annaev0410@gmail.com>
2020-02-07 19:23:52 +0300

    Push GitHub

---
+++ app/src/main/java/com/example/bookshelf_anna0410/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookshelf_anna0410;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
