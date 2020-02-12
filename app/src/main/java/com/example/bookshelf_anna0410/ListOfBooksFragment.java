f9c99b9caf3db6f6cabc014c5490950683b14af0
Anna <annaev0410@gmail.com>
2020-02-12 12:07:31 +0300

    Merged layouts to scroll through lists - fragment list of book

--- app/src/main/java/com/example/bookshelf_anna0410/ListOfBooksFragment.java
+++ app/src/main/java/com/example/bookshelf_anna0410/ListOfBooksFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 public class ListOfBooksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_list_of_books, container, false);
         return rootView;
     }
 }
