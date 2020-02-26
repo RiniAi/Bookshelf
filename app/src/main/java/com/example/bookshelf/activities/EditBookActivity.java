2e4cdf0857487efab6df14760baf1b83aeaad2d9
Anna <annaev0410@gmail.com>
2020-02-26 12:38:16 +0300

    Add EditBookActivity

---
+++ app/src/main/java/com/example/bookshelf/activities/EditBookActivity.java
@@ -0,0 +1,15 @@
+package com.example.bookshelf.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bookshelf.R;
+
+public class EditBookActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book);
+    }
+}
