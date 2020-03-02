46c8767d69ad3aa1021f7f6dd54af2e2157c970a
Anna <annaev0410@gmail.com>
2020-03-02 18:47:42 +0300

    Checking for null fields

--- app/src/main/java/com/example/bookshelf/Storage.java
+++ app/src/main/java/com/example/bookshelf/Storage.java
@@ -21,18 +21,45 @@ public class Storage {
         List<Book> booksSearch = new ArrayList<>();
         for (int i = 0; i < bookResult.size(); i++) {
             Book book = new Book();
-            book.setAuthors(bookResult.get(i).getVolumeInfo().getAuthors().toString()
-                    .replace("[", "")
-                    .replace("]", ""));
-            book.setTitle(bookResult.get(i).getVolumeInfo().getTitle());
-            book.setImageLinks(bookResult.get(i).getVolumeInfo().getImageLinks().getThumbnail());
-            book.setAverageRating(bookResult.get(i).getVolumeInfo().getAverageRating());
-            book.setPublisher(bookResult.get(i).getVolumeInfo().getPublisher());
+            if (bookResult.get(i).getVolumeInfo().getAuthors() == null)
+                book.setTitle("");
+            else
+                book.setAuthors(bookResult.get(i).getVolumeInfo().getAuthors().toString()
+                        .replace("[", "")
+                        .replace("]", ""));
+            if (bookResult.get(i).getVolumeInfo().getTitle() == null)
+                book.setTitle("");
+            else
+                book.setTitle(bookResult.get(i).getVolumeInfo().getTitle());
+            if (bookResult.get(i).getVolumeInfo().getImageLinks().getThumbnail() == null)
+                book.setImageLinks("");
+            else
+                book.setImageLinks(bookResult.get(i).getVolumeInfo().getImageLinks().getThumbnail());
+            if (bookResult.get(i).getVolumeInfo().getAverageRating() == 0)
+                book.setAverageRating(0);
+            else
+                book.setAverageRating(bookResult.get(i).getVolumeInfo().getAverageRating());
+            if (bookResult.get(i).getVolumeInfo().getPublisher() == null)
+                book.setPublisher("");
+            else
+                book.setPublisher(bookResult.get(i).getVolumeInfo().getPublisher());
             // TODO anna 28.02.2020: make a Date and convert to String
-            book.setPublishedDate(bookResult.get(i).getVolumeInfo().getPublishedDate());
-            book.setPageCount(bookResult.get(i).getVolumeInfo().getPageCount());
-            book.setLanguage(bookResult.get(i).getVolumeInfo().getLanguage());
-            book.setDescription(bookResult.get(i).getVolumeInfo().getDescription());
+            if (bookResult.get(i).getVolumeInfo().getPublishedDate() == null)
+                book.setPublishedDate("");
+            else
+                book.setPublishedDate(bookResult.get(i).getVolumeInfo().getPublishedDate());
+            if (bookResult.get(i).getVolumeInfo().getPageCount() == 0)
+                book.setPageCount(0);
+            else
+                book.setDescription(bookResult.get(i).getVolumeInfo().getDescription());
+            if (bookResult.get(i).getVolumeInfo().getLanguage() == null)
+                book.setLanguage("");
+            else
+                book.setLanguage(bookResult.get(i).getVolumeInfo().getLanguage());
+            if (bookResult.get(i).getVolumeInfo().getDescription() == null)
+                book.setDescription("");
+            else
+                book.setDescription(bookResult.get(i).getVolumeInfo().getDescription());
             booksSearch.add(book);
         }
         return booksSearch;
