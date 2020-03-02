bbd3f59a261b71cb99697fad481ecf78b875afa2
Anna <annaev0410@gmail.com>
2020-03-02 12:18:48 +0300

    Split the code in onCreate into several methods - About Book Activity

--- app/src/main/java/com/example/bookshelf/activities/AboutBookActivity.java
+++ app/src/main/java/com/example/bookshelf/activities/AboutBookActivity.java
@@ -15,25 +15,46 @@ public class AboutBookActivity extends AppCompatActivity {
     public static final String EXTRA_BOOK = "book";
     private Book book;
 
+    private TextView title;
+    private TextView author;
+    private ImageView image;
+    private RatingBar rating;
+    private TextView publishedDate;
+    private TextView publisher;
+    private TextView pageCount;
+    private TextView lang;
+    private TextView description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_book);
-        TextView title = (findViewById(R.id.tv_title_about_book));
-        TextView author = (findViewById(R.id.tv_author_about_book));
-        ImageView image = (findViewById(R.id.img_about_book));
-        RatingBar rating = (findViewById(R.id.rb_rating_about_book));
-        TextView publishedDate = (findViewById(R.id.tv_published_date_about_book));
-        TextView publisher = (findViewById(R.id.tv_publisher_about_book));
-        TextView pageCount = (findViewById(R.id.tv_page_count_about_book));
-        TextView lang = (findViewById(R.id.tv_lang_about_book));
-        TextView description = (findViewById(R.id.tv_description_about_book));
 
+        getBook();
+        initControls();
+        fillFields();
+    }
+
+    private void getBook() {
         Bundle bundle = getIntent().getExtras();
         if (bundle != null && bundle.containsKey(EXTRA_BOOK)) {
             book = (Book) bundle.getSerializable(EXTRA_BOOK);
         }
+    }
 
+    private void initControls() {
+        title = (findViewById(R.id.tv_title_about_book));
+        author = (findViewById(R.id.tv_author_about_book));
+        image = (findViewById(R.id.img_about_book));
+        rating = (findViewById(R.id.rb_rating_about_book));
+        publishedDate = (findViewById(R.id.tv_published_date_about_book));
+        publisher = (findViewById(R.id.tv_publisher_about_book));
+        pageCount = (findViewById(R.id.tv_page_count_about_book));
+        lang = (findViewById(R.id.tv_lang_about_book));
+        description = (findViewById(R.id.tv_description_about_book));
+    }
+
+    private void fillFields() {
         title.setText(book.getTitle());
         author.setText(book.getAuthors());
         Picasso.get().load(book.getImageURL()).into(image);
