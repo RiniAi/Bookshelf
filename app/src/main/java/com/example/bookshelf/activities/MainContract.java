e0a086cb3cbd624ff73dc00b56df01af3b4ecc13
Anna <annaev0410@gmail.com>
2020-03-16 15:03:23 +0300

    Add MainContract for MainActivity and MainPresenter

---
+++ app/src/main/java/com/example/bookshelf/activities/MainContract.java
@@ -0,0 +1,12 @@
+package com.example.bookshelf.activities;
+
+import com.example.bookshelf.BasePresenter;
+import com.example.bookshelf.BaseView;
+
+public interface MainContract {
+    interface View extends BaseView {
+    }
+
+    interface Presenter extends BasePresenter {
+    }
+}
