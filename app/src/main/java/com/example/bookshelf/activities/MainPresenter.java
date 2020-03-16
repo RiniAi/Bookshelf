7257254ffa2c7302a078d35b1439b1f67f0873e1
Anna <annaev0410@gmail.com>
2020-03-16 12:37:49 +0300

    Create Presenter for MainActivity

---
+++ app/src/main/java/com/example/bookshelf/activities/MainPresenter.java
@@ -0,0 +1,9 @@
+package com.example.bookshelf.activities;
+
+public class MainPresenter {
+    private MainActivity mainActivity;
+
+    public MainPresenter(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+}
