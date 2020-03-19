3795e2924d43e56232c835a57080e8eac41662be
Anna <annaev0410@gmail.com>
2020-03-19 17:37:12 +0300

    Add EditBookContract and EditBookPresenter classes for Migrate to MVP:EditBookActivity

---
+++ app/src/main/java/com/example/bookshelf/features/bookedit/EditBookContract.java
@@ -0,0 +1,12 @@
+package com.example.bookshelf.features.bookedit;
+
+import com.example.bookshelf.base.BasePresenter;
+import com.example.bookshelf.base.BaseView;
+
+public interface EditBookContract {
+    interface View extends BaseView {
+    }
+
+    interface Presenter extends BasePresenter {
+    }
+}
---
+++ app/src/main/java/com/example/bookshelf/features/bookedit/EditBookPresenter.java
@@ -0,0 +1,9 @@
+package com.example.bookshelf.features.bookedit;
+
+public class EditBookPresenter implements EditBookContract.Presenter {
+
+    @Override
+    public void onStart() {
+
+    }
+}
