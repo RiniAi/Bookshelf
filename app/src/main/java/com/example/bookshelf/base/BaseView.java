528eef4ddcd331c4a6745e84aceaab085072c15d
Anna <annaev0410@gmail.com>
2020-03-26 15:07:47 +0300

    Change BookStorage

---
+++ app/src/main/java/com/example/bookshelf/base/BaseView.java
@@ -0,0 +1,4 @@
+package com.example.bookshelf.base;
+
+public interface BaseView {
+}
--- app/src/main/java/com/example/bookshelf/database/BookStorage.java
+++ app/src/main/java/com/example/bookshelf/database/BookStorage.java
@@ -1,32 +1,25 @@
 package com.example.bookshelf.database;
 
-import com.example.bookshelf.App;
-
 import java.util.List;
 
 import javax.inject.Inject;
 
 public class BookStorage {
-
     @Inject
     BookDao bookDao;
-
-    public void onStart() {
-        App.getAppComponent().injectBookStorage(this);
+    @Inject
+    public BookStorage() {
     }
 
     public Book searchBookDb(Book book) {
-        onStart();
         return bookDao.findBookTitleAndAuthor(book.title, book.authors);
     }
 
     public List<Book> getAllWithStatus(Book.BookStatus status) {
-        onStart();
         return bookDao.getAllWithStatus(BookStatusConverter.fromStatusToString(status));
     }
 
     public List<Book> getAll() {
-        onStart();
         return bookDao.getAll();
     }
 
