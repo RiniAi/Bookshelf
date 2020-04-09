141089bdfc45f56164be0e7ebdd0312a6e8afbfe
Anna <annaev0410@gmail.com>
2020-04-09 12:45:03 +0300

    Add unit tests for EditBookPresenter

--- app/src/test/java/com/example/bookshelf/features/bookedit/EditBookPresenterTest.java
+++ app/src/test/java/com/example/bookshelf/features/bookedit/EditBookPresenterTest.java
@@ -2,21 +2,43 @@ package com.example.bookshelf.features.bookedit;
 
 import android.os.Bundle;
 
+import com.example.bookshelf.database.Book;
+import com.example.bookshelf.usecases.SearchBookUseCase;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 class EditBookPresenterTest {
     @InjectMocks
     EditBookPresenter presenter;
-    @Mock
-    EditBookContract.View view;
-    @Mock
-    Bundle bundle;
+    private SearchBookUseCase.Params searchParams = mock(SearchBookUseCase.Params.class);
+    private SearchBookUseCase useCase = mock(SearchBookUseCase.class);
+    private EditBookContract.View view = mock(EditBookContract.View.class);
+    private Bundle bundle = mock(Bundle.class);
+    private Book book;
+
+    @BeforeEach
+    void prepareData() {
+        presenter = new EditBookPresenter(useCase, view);
+    }
 
     @Test
-    void editBookPresenterSetSomeDate() {
+    void editBookPresenterLoadBookBundleIsEmpty() {
+        presenter.onStartWithData(bundle);
+        verify(view).showErrorMessage();
+    }
 
+    @Test
+    void editBookPresenterSearchBook() {
+        presenter.onStartWithData(bundle);
+        useCase.run(searchParams);
+        when(useCase.run(searchParams)).thenReturn(book);
+        verify(view).hideButtonDelete();
     }
 }
