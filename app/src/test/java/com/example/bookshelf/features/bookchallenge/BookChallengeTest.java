70c441e8105ed61004e19e342f4edce4f1e66c35
Anna <annaev0410@gmail.com>
2020-04-14 17:12:14 +0300

    Add use of @ExtendWith(MockitoExtension.class) and fix some test

--- app/src/test/java/com/example/bookshelf/features/bookchallenge/BookChallengeTest.java
+++ app/src/test/java/com/example/bookshelf/features/bookchallenge/BookChallengeTest.java
@@ -8,51 +8,59 @@ import com.example.bookshelf.usecases.SearchBookWithStatusUseCase;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
+import static com.example.bookshelf.features.bookchallenge.BookChallengePresenter.STORAGE_COUNTER;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BookChallengeTest {
     @InjectMocks
     BookChallengePresenter presenter;
-    private BookChallengeContract.View view = mock(BookChallengeContract.View.class);
-    private SearchBookWithStatusUseCase useCase = mock(SearchBookWithStatusUseCase.class);
-    private SharedPreferences sharedPreferences = mock(SharedPreferences.class);
-    private Navigator navigator = mock(Navigator.class);
-    private String counter;
+    @Mock
+    BookChallengeContract.View view;
+    @Mock
+    SearchBookWithStatusUseCase useCase;
+    @Mock
+    SharedPreferences sharedPreferences;
+    @Mock
+    Navigator navigator;
+
     private List<Book> books;
-    private Book book = new Book();
+    private Book book;
 
     @BeforeEach
-    void prepareData() {
-        presenter = new  BookChallengePresenter(useCase, sharedPreferences, navigator, view);
-        counter = null;
+    void setUp() {
+        presenter.setView(view);
         books = new ArrayList<>();
+        book = new Book();
     }
 
     @Test
-    void bookChallengePresenterLoadNullCounter(){
-        presenter.onStart();
-        verify(view).changeCounter(counter);
-        verify(view).changeCounterForBar(0);
-    }
-
-    @Test
-    void bookChallengePresenterViewLoadNotNullCounter(){
-        counter = "5";
-        when(sharedPreferences.getString(BookChallengePresenter.STORAGE_COUNTER, "0")).thenReturn(counter);
+    void bookChallengePresenterLoadCounter() {
+        String counter = "5";
+        when(sharedPreferences.getString(STORAGE_COUNTER, "0")).thenReturn(counter);
         presenter.onStart();
         verify(view).changeCounter(counter);
         verify(view).changeCounterForBar(5);
     }
 
     @Test
-    void bookChallengePresenterLoadEmptyList(){
+    void bookChallengePresenterLoadNullCounter() {
+        presenter.onStart();
+        verify(view).changeCounter(null);
+        verify(view).changeCounterForBar(0);
+    }
+
+    @Test
+    void bookChallengePresenterLoadEmptyList() {
         when(useCase.run()).thenReturn(books);
         presenter.onStart();
         verify(useCase).run();
@@ -60,7 +68,7 @@ class BookChallengeTest {
     }
 
     @Test
-    void bookChallengePresenterLoadNotEmptyList(){
+    void bookChallengePresenterLoadNotEmptyList() {
         books.add(book);
         when(useCase.run()).thenReturn(books);
         presenter.onStart();
@@ -70,31 +78,19 @@ class BookChallengeTest {
     }
 
     @Test
-    void bookChallengePresenterChangeNotNullCounter(){
+    void bookChallengePresenterChangeCounter() {
         presenter.onProgressChanged(5);
         verify(view).changeCounter("5");
     }
 
     @Test
-    void bookChallengePresenterChangeNullCounter(){
-        presenter.onProgressChanged(0);
-        verify(view).changeCounter("0");
-    }
-
-    @Test
-    public void bookChallengePresenterOpenBook() {
+    void bookChallengePresenterOpenBook() {
         presenter.openBook(book);
         verify(navigator).openBook(book);
     }
 
     @Test
-    public void bookChallengePresenterOpenNull() {
-        presenter.openBook(null);
-        verify(navigator).openBook(null);
-    }
-
-    @Test
-    public void bookChallengePresenterOpenMain() {
+    void bookChallengePresenterOpenMain() {
         presenter.openMain();
         verify(navigator).openMain();
     }
