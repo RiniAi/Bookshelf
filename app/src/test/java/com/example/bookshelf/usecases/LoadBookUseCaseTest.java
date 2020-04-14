187ba93cc2ed9b3995c51c4cf972b64ce415a8c6
Anna <annaev0410@gmail.com>
2020-04-14 12:29:15 +0300

    Add @ExtendWith(MockitoExtension.class) for LoadBookUseCaseTest

--- app/src/test/java/com/example/bookshelf/usecases/LoadBookUseCaseTest.java
+++ app/src/test/java/com/example/bookshelf/usecases/LoadBookUseCaseTest.java
@@ -3,31 +3,28 @@ package com.example.bookshelf.usecases;
 import com.example.bookshelf.database.Book;
 import com.example.bookshelf.database.BookStorage;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class LoadBookUseCaseTest {
     @InjectMocks
     LoadBookUseCase useCase;
-    private BookStorage storage = mock(BookStorage.class);
-    private List<Book> books;
-
-    @BeforeEach
-    void prepareData() {
-        useCase = new LoadBookUseCase(storage);
-        books = new ArrayList<>();
-    }
+    @Mock
+    BookStorage storage;
 
     @Test
     void loadBookUseCaseCorrectDataLoading() {
+        List<Book> books = new ArrayList<>();
         when(useCase.run()).thenReturn(books);
         useCase.run();
         verify(storage).getAll();
