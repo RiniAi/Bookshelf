085f3bd3e95805974f61f3fcff07d44c1b7982c5
Anna <annaev0410@gmail.com>
2020-04-14 12:29:35 +0300

    Add @ExtendWith(MockitoExtension.class) for RequestBooksUseCaseTest

--- app/src/test/java/com/example/bookshelf/usecases/RequestBooksUseCaseTest.java
+++ app/src/test/java/com/example/bookshelf/usecases/RequestBooksUseCaseTest.java
@@ -4,20 +4,24 @@ import com.example.bookshelf.features.bookssearch.Repository;
 import com.example.bookshelf.features.bookssearch.SearchCall.ResponseListener;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class RequestBooksUseCaseTest {
     @InjectMocks
     RequestBooksUseCase useCase;
-    private Repository repository = mock(Repository.class);
-    private ResponseListener listener = mock(ResponseListener.class);
+    @Mock
+    Repository repository;
+    @Mock
+    ResponseListener listener;
 
     @Test
     void requestBooksUseCaseRepositoryCorrectCall() {
-        useCase = new RequestBooksUseCase(repository);
         useCase.run("Tom", listener);
         verify(repository).requestBooksFromApi("Tom", listener);
     }
