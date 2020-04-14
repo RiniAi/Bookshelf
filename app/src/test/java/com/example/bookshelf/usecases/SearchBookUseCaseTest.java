b0de63a41238757d7f9d99e954a4317e840b5ace
Anna <annaev0410@gmail.com>
2020-04-14 12:29:48 +0300

    Add @ExtendWith(MockitoExtension.class) for SearchBookUseCaseTest

--- app/src/test/java/com/example/bookshelf/usecases/SearchBookUseCaseTest.java
+++ app/src/test/java/com/example/bookshelf/usecases/SearchBookUseCaseTest.java
@@ -4,37 +4,28 @@ import com.example.bookshelf.database.Book;
 import com.example.bookshelf.database.BookStorage;
 import com.example.bookshelf.usecases.SearchBookUseCase.Params;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SearchBookUseCaseTest {
     @InjectMocks
     SearchBookUseCase useCase;
-    private BookStorage storage = mock(BookStorage.class);
-    private Book book = new Book();
-
-    @BeforeEach
-    void prepareData() {
-        useCase = new SearchBookUseCase(storage);
-    }
+    @Mock
+    BookStorage storage;
 
     @Test
     void searchBookUseCaseSearchBook() {
+        Book book = new Book();
         Params params = new Params(book);
         when(useCase.run(params)).thenReturn(book);
         useCase.run(params);
         verify(storage).search(params.getBook());
-    }
-
-    @Test
-    void searchBookUseCaseCaseSearchNull() {
-        Params params = new Params(null);
-        when(useCase.run(params)).thenReturn(null);
-        useCase.run(params);
     }
 }
