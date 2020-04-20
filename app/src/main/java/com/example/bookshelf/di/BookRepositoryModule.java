ec180cb9a4fa71ac5087bacefc18e948c5e7040c
Anna <annaev0410@gmail.com>
2020-04-20 17:16:30 +0300

    BookRepository: add GoogleBooksApiService dependency

--- app/src/main/java/com/example/bookshelf/di/BookRepositoryModule.java
+++ app/src/main/java/com/example/bookshelf/di/BookRepositoryModule.java
@@ -2,6 +2,7 @@ package com.example.bookshelf.di;
 
 import com.example.bookshelf.features.bookssearch.BookRepository;
 import com.example.bookshelf.features.bookssearch.Repository;
+import com.example.bookshelf.network.GoogleBooksApiService;
 
 import dagger.Module;
 import dagger.Provides;
@@ -9,7 +10,7 @@ import dagger.Provides;
 @Module
 public class BookRepositoryModule {
     @Provides
-    public Repository providesBookStorage () {
-        return new BookRepository();
+    public Repository providesBookStorage(GoogleBooksApiService service) {
+        return new BookRepository(service);
     }
 }
--- app/src/main/java/com/example/bookshelf/features/bookssearch/BookRepository.java
+++ app/src/main/java/com/example/bookshelf/features/bookssearch/BookRepository.java
@@ -2,7 +2,6 @@ package com.example.bookshelf.features.bookssearch;
 
 import com.example.bookshelf.database.Book;
 import com.example.bookshelf.network.GoogleBooksApiService;
-import com.example.bookshelf.network.RetrofitClientInstance;
 
 import java.util.List;
 
@@ -15,10 +14,14 @@ import static com.example.bookshelf.network.GoogleBooksApiService.QUERY_COUNTER;
 
 public class BookRepository implements Repository {
     @Inject
-    public BookRepository() {
+    GoogleBooksApiService service;
+
+    @Inject
+    public BookRepository(GoogleBooksApiService service) {
+        this.service = service;
     }
+
     public Single<List<Book>> getBooks(String query) {
-        GoogleBooksApiService service = RetrofitClientInstance.getRetrofitInstance().create(GoogleBooksApiService.class);
         return service.getBooks(query, QUERY_COUNTER)
                 .map(booksApiResponse -> mapResponseToDomain(booksApiResponse));
     }
