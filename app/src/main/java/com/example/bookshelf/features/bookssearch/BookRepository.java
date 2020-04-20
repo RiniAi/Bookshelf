cc255d80b08633628f35ac770e0ecb3f3b692282
Anna <annaev0410@gmail.com>
2020-04-20 17:12:09 +0300

    BookRepository: change the return value with void to Single and change getBooks method using RxJava

--- app/src/main/java/com/example/bookshelf/features/bookssearch/BookRepository.java
+++ app/src/main/java/com/example/bookshelf/features/bookssearch/BookRepository.java
@@ -1,20 +1,14 @@
 package com.example.bookshelf.features.bookssearch;
 
-import com.example.bookshelf.models.BooksApiResponse;
-import com.example.bookshelf.models.BooksApiResponseItem;
+import com.example.bookshelf.database.Book;
 import com.example.bookshelf.network.GoogleBooksApiService;
 import com.example.bookshelf.network.RetrofitClientInstance;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import io.reactivex.rxjava3.core.Single;
 
 import static com.example.bookshelf.network.BookMapper.mapResponseToDomain;
 import static com.example.bookshelf.network.GoogleBooksApiService.QUERY_COUNTER;
@@ -23,23 +17,9 @@ public class BookRepository implements Repository {
     @Inject
     public BookRepository() {
     }
-    public void requestBooksFromApi(String query, SearchCall.ResponseListener responseListener) {
+    public Single<List<Book>> getBooks(String query) {
         GoogleBooksApiService service = RetrofitClientInstance.getRetrofitInstance().create(GoogleBooksApiService.class);
-        Call<BooksApiResponse> call = service.getBooks(query, QUERY_COUNTER);
-        call.enqueue(new Callback<BooksApiResponse>() {
-            @Override
-            public void onResponse(@NotNull Call<BooksApiResponse> call, @NotNull Response<BooksApiResponse> response) {
-                List<BooksApiResponseItem> booksResponse = new ArrayList<>();
-                if (response.body() != null) {
-                    booksResponse = response.body().getItems();
-                }
-                responseListener.onSuccess(mapResponseToDomain(booksResponse));
-            }
-
-            @Override
-            public void onFailure(@NotNull Call<BooksApiResponse> call, @NotNull Throwable t) {
-                responseListener.onFailure(t);
-            }
-        });
+        return service.getBooks(query, QUERY_COUNTER)
+                .map(booksApiResponse -> mapResponseToDomain(booksApiResponse));
     }
 }
