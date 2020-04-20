c26aaa70279aba9fe47ffe01b3b0c62f72b0801e
Anna <annaev0410@gmail.com>
2020-04-20 17:10:12 +0300

    BookMapper: change mapResponseToDomain method arguments: with List<BooksApiResponseItem> to BooksApiResponse

--- app/src/main/java/com/example/bookshelf/network/BookMapper.java
+++ app/src/main/java/com/example/bookshelf/network/BookMapper.java
@@ -1,35 +1,35 @@
 package com.example.bookshelf.network;
 
 import com.example.bookshelf.database.Book;
-import com.example.bookshelf.models.BooksApiResponseItem;
+import com.example.bookshelf.models.BooksApiResponse;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookMapper {
 
-    public static List<Book> mapResponseToDomain(List<BooksApiResponseItem> bookResult) {
+    public static List<Book> mapResponseToDomain(BooksApiResponse bookResult) {
         List<Book> booksSearch = new ArrayList<>();
-        for (int i = 0; i < bookResult.size(); i++) {
+        for (int i = 0; i < bookResult.getItems().size(); i++) {
             Book book = new Book();
-            if (bookResult.get(i).getVolumeInfo().getAuthors() == null) {
+            if (bookResult.getItems().get(i).getVolumeInfo().getAuthors() == null) {
                 book.setAuthors("");
             } else {
-                book.setAuthors(bookResult.get(i).getVolumeInfo().getAuthors().toString()
+                book.setAuthors(bookResult.getItems().get(i).getVolumeInfo().getAuthors().toString()
                         .replace("[", "")
                         .replace("]", ""));
             }
-            book.setTitle(bookResult.get(i).getVolumeInfo().getTitle());
-            if (bookResult.get(i).getVolumeInfo().getImageLinks() != null
-                    && bookResult.get(i).getVolumeInfo().getImageLinks().getThumbnail() != null) {
-                book.setImageLinks(bookResult.get(i).getVolumeInfo().getImageLinks().getThumbnail());
+            book.setTitle(bookResult.getItems().get(i).getVolumeInfo().getTitle());
+            if (bookResult.getItems().get(i).getVolumeInfo().getImageLinks() != null
+                    && bookResult.getItems().get(i).getVolumeInfo().getImageLinks().getThumbnail() != null) {
+                book.setImageLinks(bookResult.getItems().get(i).getVolumeInfo().getImageLinks().getThumbnail());
             }
-            book.setAverageRating(bookResult.get(i).getVolumeInfo().getAverageRating());
-            book.setPublisher(bookResult.get(i).getVolumeInfo().getPublisher());
-            book.setPublishedDate(bookResult.get(i).getVolumeInfo().getPublishedDate());
-            book.setPageCount(bookResult.get(i).getVolumeInfo().getPageCount());
-            book.setLanguage(bookResult.get(i).getVolumeInfo().getLanguage());
-            book.setDescription(bookResult.get(i).getVolumeInfo().getDescription());
+            book.setAverageRating(bookResult.getItems().get(i).getVolumeInfo().getAverageRating());
+            book.setPublisher(bookResult.getItems().get(i).getVolumeInfo().getPublisher());
+            book.setPublishedDate(bookResult.getItems().get(i).getVolumeInfo().getPublishedDate());
+            book.setPageCount(bookResult.getItems().get(i).getVolumeInfo().getPageCount());
+            book.setLanguage(bookResult.getItems().get(i).getVolumeInfo().getLanguage());
+            book.setDescription(bookResult.getItems().get(i).getVolumeInfo().getDescription());
 
             booksSearch.add(book);
         }
