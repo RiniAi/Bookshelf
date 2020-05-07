89ada8f76c2fbe449e7d96d14620ead44538feb1
Anna <annaev0410@gmail.com>
2020-05-07 12:46:29 +0300

    Remove extra page navigation methods for Navigator

--- app/src/main/java/com/example/bookshelf/Navigator.java
+++ app/src/main/java/com/example/bookshelf/Navigator.java
@@ -6,11 +6,8 @@ import android.content.Intent;
 import com.example.bookshelf.database.Book;
 import com.example.bookshelf.features.bookabout.AboutBookActivity;
 import com.example.bookshelf.features.bookabout.AboutBookPresenter;
-import com.example.bookshelf.features.bookchallenge.BookChallengeActivity;
 import com.example.bookshelf.features.bookedit.EditBookActivity;
 import com.example.bookshelf.features.bookedit.EditBookPresenter;
-import com.example.bookshelf.features.bookssearch.SearchActivity;
-import com.example.bookshelf.features.main.MainActivity;
 
 import javax.inject.Inject;
 
@@ -31,21 +28,6 @@ public class Navigator {
     public void editBook(Book book) {
         Intent intent = new Intent(context, EditBookActivity.class);
         intent.putExtra(EditBookPresenter.EXTRA_BOOK, book);
-        context.startActivity(intent);
-    }
-
-    public void openMain() {
-        Intent intent = new Intent(context, MainActivity.class);
-        context.startActivity(intent);
-    }
-
-    public void openBookChallenge() {
-        Intent intent = new Intent(context, BookChallengeActivity.class);
-        context.startActivity(intent);
-    }
-
-    public void openSearch() {
-        Intent intent = new Intent(context, SearchActivity.class);
         context.startActivity(intent);
     }
 }
