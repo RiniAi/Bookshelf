4ea1600cc662864c466dc69b5d43185c6b5bcefa
Anna <annaev0410@gmail.com>
2020-05-22 21:48:27 +0300

    Add open AuthenticationActivity

--- app/src/main/java/com/example/bookshelf/Navigator.java
+++ app/src/main/java/com/example/bookshelf/Navigator.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import com.example.bookshelf.database.book.Book;
 import com.example.bookshelf.features.MainActivity;
+import com.example.bookshelf.features.authentication.LoginRegisterActivity;
 import com.example.bookshelf.features.bookedit.EditBookActivity;
 import com.example.bookshelf.features.bookedit.EditBookPresenter;
 
@@ -26,6 +27,11 @@ public class Navigator {
 
     public void openMain() {
         Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    public void openAuthentication() {
+        Intent intent = new Intent(context, LoginRegisterActivity.class);
         context.startActivity(intent);
     }
 }
